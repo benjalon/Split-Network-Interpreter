f20d2a66c9aec7cc655dab6823209c7286aae1e5
Ben Longhurst <benjalon@gmail.com>
2019-07-26 10:42:31 +0100

    implemented Threading

--- src/msa_processor.py
+++ src/msa_processor.py
@@ -1,7 +1,10 @@
+# pylint: disable=no-member
 """Class which takes nexus file path and produces a
  processed & coloured MSA."""
 import re
 import operator
+import time
+import concurrent.futures
 from nexus import NexusReader
 from numpy import zeros
 
@@ -9,18 +12,24 @@ from numpy import zeros
 class MsaProcessor():
     """Processes a nexus file."""
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, top_splits=None):
         nexus_file = NexusReader(file_path)
 
-        self._msa = nexus_file.characters.matrix  # pylint: disable=no-member
-        self.num_columns = nexus_file.characters.nchar  # pylint: disable=no-member
-        self.num_species = nexus_file.characters.ntaxa  # pylint: disable=no-member
-        self.symbols = nexus_file.characters.symbols  # pylint:disable=no-member
-        total_splits = self._process_splits(nexus_file.splits.block)  # pylint: disable=no-member
+        self._msa = nexus_file.characters.matrix
+        self.num_columns = nexus_file.characters.nchar
+        self.num_species = nexus_file.characters.ntaxa
+        self.symbols = nexus_file.characters.symbols
+        total_splits = self._process_splits(
+            nexus_file.splits.block)
         self.upper_limit = 0
-        total_splits = sorted(total_splits, key=operator.itemgetter('split_weight'), reverse=True)
-        self.splits = total_splits[0:required_splits]
-        self._calculate()
+        total_splits = sorted(
+            total_splits,
+            key=operator.itemgetter('split_weight'), reverse=True)
+        if top_splits is None:
+            self.splits = total_splits
+        else:
+            self.splits = total_splits[0:top_splits]
+        print("Total time: {}".format(self._calculate()))
 
     def _process_splits(self, block):
         """Returns a list of splits e.g [1,2,3],[1],[3,4]"""
@@ -32,12 +41,12 @@ class MsaProcessor():
                 reg = r"\[(.*),.*TAB\s(.*)\sTAB\s*(.*),"
                 data_row = data_row.replace("\t", "TAB")
                 split = re.findall(reg, data_row)
-                sp = list(map(int, split[0][2].split(" ")))
+                splits_int = list(map(int, split[0][2].split(" ")))
                 split_dict = {
                     'split_number': int(split[0][0]),
                     'split_weight': float(split[0][1]),
-                    'split': sp,
-                    'inverse': self._get_inverse_split(sp)
+                    'split': splits_int,
+                    'inverse': self._get_inverse_split(splits_int)
                 }
                 split_list.append(split_dict)
 
@@ -78,7 +87,8 @@ class MsaProcessor():
 
                     if(set_info['pTogether'] and set_info['qTogether']):
                         rand_s += 1
-                    if (not set_info['pTogether'] and not set_info['qTogether']):
+                    if (not set_info['pTogether'] and not
+                            set_info['qTogether']):
                         rand_r += 1
                     if (not set_info['pTogether'] and set_info['qTogether']):
                         rand_u += 1
@@ -130,27 +140,41 @@ class MsaProcessor():
 
         return pos
 
-    def _calculate(self):
+    def _calculate(self, threading=True):
         """Does the main logic"""
+        start = time.time()
+        total_columns = list(range(self.num_columns))
+        if threading:
+            with concurrent.futures.ProcessPoolExecutor() as executor:
+                for col_num, result in zip(
+                        total_columns, executor.map(
+                            self._calc2, total_columns)):
 
-        for col_num in range(self.num_columns):
+                    print("Column: {} - Result {}".format(col_num, result))
+        else:
+            for col_num in total_columns:
+                print("Column: {} - Result {}".format(
+                    col_num, self._calc2(col_num)))
 
-            # Get the column of the MSA.
-            column = []
-            for species in self._msa:
-                column.append(self._msa[species][col_num])
+        return time.time() - start
 
-            # As we use a Set, if there are any duplicates, the length will
-            # greater than 1 meaning the column is a partition.
-            if len(set(column)) > 1:
+    def _calc2(self, col_num):
+        # Get the column of the MSA.
+        column = []
+        for species in self._msa:
+            column.append(self._msa[species][col_num])
 
-                # Now make the column into a parition referencing species
-                # number. e.g. (1:A,2:A,3:G,4:A) would be (1,2,4) - (3)
-                partition = self._make_sets_from_column(column)
-                result = self._match_split(partition)
-                print(result)
-            else:
-                print("Row ", col_num+1, "= False")
+        # As we use a Set, if there are any duplicates, the length will
+        # greater than 1 meaning the column is a partition.
+        if len(set(column)) > 1:
+
+            # Now make the column into a parition referencing species
+            # number. e.g. (1:A,2:A,3:G,4:A) would be (1,2,4) - (3)
+            partition = self._make_sets_from_column(column)
+            result = self._match_split(partition)
+            return result
+        else:
+            return "N/A"
 
     def msa(self):
         """Returns the MSA as a matrix."""
@@ -158,4 +182,6 @@ class MsaProcessor():
     def split_columns(self):
         """Returns an array containing the split found at each location."""
 
-PRO = MsaProcessor('/Users/benlonghurst/Documents/GitHub/Split-Network-Interpreter/Nexus_Examples/mammals.nex')
+PATH = '/Users/benlonghurst/Documents/GitHub/'
+PATH = PATH + 'Split-Network-Interpreter/Nexus_Examples/'
+PRO = MsaProcessor(PATH+'mammals.nex', 10)
